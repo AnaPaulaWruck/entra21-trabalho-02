adfe02bf5d39b05e7481354911a7cc281002d858
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-06-28 23:40:33 -0300

    Atualizacao cliente

---
+++ TrabalhoWindowsForms01/ClinicaVeterinaria/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoWindowsForms01.ClinicaVeterinaria
+{
+    internal class Cliente
+    {
+    }
+}
