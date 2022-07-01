0c1f1d31b472cc213fc632f8c3e595650f4d0caf
Ana Paula Wruck <wruck.ap@gmail.com>
2022-07-01 04:32:05 -0300

    Ajustado design do menu principal

--- TrabalhoWindowsForms01/ClinicaVeterinaria/MenuPrincipalForm.Designer.cs
+++ TrabalhoWindowsForms01/ClinicaVeterinaria/MenuPrincipalForm.Designer.cs
@@ -38,82 +38,117 @@
             // 
             // buttonConsultas
             // 
-            this.buttonConsultas.Location = new System.Drawing.Point(22, 24);
-            this.buttonConsultas.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonConsultas.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonConsultas.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonConsultas.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonConsultas.ForeColor = System.Drawing.Color.Navy;
+            this.buttonConsultas.Image = global::TrabalhoWindowsForms01.Properties.Resources.consulta2;
+            this.buttonConsultas.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonConsultas.Location = new System.Drawing.Point(19, 18);
             this.buttonConsultas.Name = "buttonConsultas";
-            this.buttonConsultas.Size = new System.Drawing.Size(143, 167);
+            this.buttonConsultas.Size = new System.Drawing.Size(160, 165);
             this.buttonConsultas.TabIndex = 0;
             this.buttonConsultas.Text = "CONSULTAS";
-            this.buttonConsultas.UseVisualStyleBackColor = true;
+            this.buttonConsultas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonConsultas.UseVisualStyleBackColor = false;
             this.buttonConsultas.Click += new System.EventHandler(this.buttonConsultas_Click);
             // 
             // buttonPets
             // 
-            this.buttonPets.Location = new System.Drawing.Point(183, 24);
-            this.buttonPets.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonPets.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonPets.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonPets.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonPets.ForeColor = System.Drawing.Color.Navy;
+            this.buttonPets.Image = global::TrabalhoWindowsForms01.Properties.Resources.pet2;
+            this.buttonPets.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonPets.Location = new System.Drawing.Point(197, 18);
             this.buttonPets.Name = "buttonPets";
-            this.buttonPets.Size = new System.Drawing.Size(143, 167);
+            this.buttonPets.Size = new System.Drawing.Size(160, 165);
             this.buttonPets.TabIndex = 1;
             this.buttonPets.Text = "PETS";
-            this.buttonPets.UseVisualStyleBackColor = true;
+            this.buttonPets.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonPets.UseVisualStyleBackColor = false;
             this.buttonPets.Click += new System.EventHandler(this.buttonPets_Click);
             // 
             // buttonClientes
             // 
-            this.buttonClientes.Location = new System.Drawing.Point(344, 24);
-            this.buttonClientes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonClientes.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonClientes.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonClientes.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonClientes.ForeColor = System.Drawing.Color.Navy;
+            this.buttonClientes.Image = global::TrabalhoWindowsForms01.Properties.Resources.cliente1;
+            this.buttonClientes.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonClientes.Location = new System.Drawing.Point(375, 18);
             this.buttonClientes.Name = "buttonClientes";
-            this.buttonClientes.Size = new System.Drawing.Size(143, 167);
+            this.buttonClientes.Size = new System.Drawing.Size(160, 165);
             this.buttonClientes.TabIndex = 2;
             this.buttonClientes.Text = "CLIENTES";
-            this.buttonClientes.UseVisualStyleBackColor = true;
+            this.buttonClientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonClientes.UseVisualStyleBackColor = false;
             this.buttonClientes.Click += new System.EventHandler(this.buttonClientes_Click);
             // 
             // buttonVeterinarios
             // 
-            this.buttonVeterinarios.Location = new System.Drawing.Point(22, 217);
-            this.buttonVeterinarios.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonVeterinarios.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonVeterinarios.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonVeterinarios.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonVeterinarios.ForeColor = System.Drawing.Color.Navy;
+            this.buttonVeterinarios.Image = global::TrabalhoWindowsForms01.Properties.Resources.vet1;
+            this.buttonVeterinarios.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonVeterinarios.Location = new System.Drawing.Point(19, 200);
             this.buttonVeterinarios.Name = "buttonVeterinarios";
-            this.buttonVeterinarios.Size = new System.Drawing.Size(143, 167);
+            this.buttonVeterinarios.Size = new System.Drawing.Size(160, 165);
             this.buttonVeterinarios.TabIndex = 3;
             this.buttonVeterinarios.Text = "VETERINÁRIOS";
-            this.buttonVeterinarios.UseVisualStyleBackColor = true;
+            this.buttonVeterinarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonVeterinarios.UseVisualStyleBackColor = false;
             this.buttonVeterinarios.Click += new System.EventHandler(this.buttonVeterinarios_Click);
             // 
             // buttonAlergias
             // 
-            this.buttonAlergias.Location = new System.Drawing.Point(183, 217);
-            this.buttonAlergias.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonAlergias.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonAlergias.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonAlergias.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonAlergias.ForeColor = System.Drawing.Color.Navy;
+            this.buttonAlergias.Image = global::TrabalhoWindowsForms01.Properties.Resources.alergia1;
+            this.buttonAlergias.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonAlergias.Location = new System.Drawing.Point(197, 200);
             this.buttonAlergias.Name = "buttonAlergias";
-            this.buttonAlergias.Size = new System.Drawing.Size(143, 167);
+            this.buttonAlergias.Size = new System.Drawing.Size(160, 165);
             this.buttonAlergias.TabIndex = 4;
             this.buttonAlergias.Text = "ALERGIAS";
-            this.buttonAlergias.UseVisualStyleBackColor = true;
+            this.buttonAlergias.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonAlergias.UseVisualStyleBackColor = false;
             this.buttonAlergias.Click += new System.EventHandler(this.buttonAlergias_Click);
             // 
             // buttonEnderecos
             // 
-            this.buttonEnderecos.Location = new System.Drawing.Point(344, 217);
-            this.buttonEnderecos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonEnderecos.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonEnderecos.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonEnderecos.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonEnderecos.ForeColor = System.Drawing.Color.Navy;
+            this.buttonEnderecos.Image = global::TrabalhoWindowsForms01.Properties.Resources.endereco1;
+            this.buttonEnderecos.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonEnderecos.Location = new System.Drawing.Point(375, 200);
             this.buttonEnderecos.Name = "buttonEnderecos";
-            this.buttonEnderecos.Size = new System.Drawing.Size(143, 167);
+            this.buttonEnderecos.Size = new System.Drawing.Size(160, 165);
             this.buttonEnderecos.TabIndex = 5;
             this.buttonEnderecos.Text = "ENDEREÇOS";
-            this.buttonEnderecos.UseVisualStyleBackColor = true;
+            this.buttonEnderecos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonEnderecos.UseVisualStyleBackColor = false;
             this.buttonEnderecos.Click += new System.EventHandler(this.buttonEnderecos_Click);
             // 
             // MenuPrincipalForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 412);
+            this.ClientSize = new System.Drawing.Size(552, 381);
             this.Controls.Add(this.buttonEnderecos);
             this.Controls.Add(this.buttonAlergias);
             this.Controls.Add(this.buttonVeterinarios);
             this.Controls.Add(this.buttonClientes);
             this.Controls.Add(this.buttonPets);
             this.Controls.Add(this.buttonConsultas);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MenuPrincipalForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
